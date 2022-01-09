5423b47d33de90bef95ebb2c42cad07963c00dba
kofi-teddy <agudogot@gmail.com>
2022-01-09 20:31:00 +0000

    product models fixes with translations

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,27 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(_('Name'), max_length=200, db_index=True)
     slug = models.CharField(max_length=200, unique=True)
 
     class Meta:
         ordering = ('name',)
-        verbose_name = ('category')
-        verbose_name_plural = ('categories')
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
 
     def __str__(self) -> str:
         return self.name
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, db_index=True)
+    category = models.ForeignKey(Category, verbose_name=_("category"), related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(_('Name'), max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
+    image = models.ImageField(_('Image'), upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(_('Description'), blank=True)
+    price = models.DecimalField(_('Price'), max_digits=10, decimal_places=2)
+    available = models.BooleanField(_('Available'), default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
