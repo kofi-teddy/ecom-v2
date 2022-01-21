1a389f2dfa817d58efb24c23f655d803fd02e616
kofi-teddy <agudogot@gmail.com>
2022-01-21 16:33:20 +0000

    categories added to context on views product list

--- core/settings.py
+++ core/settings.py
@@ -102,8 +102,8 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
