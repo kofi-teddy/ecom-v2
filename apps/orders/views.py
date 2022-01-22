bbfbb2d592a0cb9cd0b75131e2e66b85dff689b3
kofi-teddy <agudogot@gmail.com>
2022-01-22 13:58:41 +0000

    calling asynchronous task on order create view

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from apps.cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 
 def order_create(request):
@@ -17,6 +18,8 @@ def order_create(request):
                 )
             # clear the cart 
             cart.clear()
+            # launch asychronous task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order, 'cart': cart})
     else:
         form = OrderCreateForm()
