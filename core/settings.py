2e82425b0a3d137ec57547c0ffc0340e021fc606
kofi-teddy <agudogot@gmail.com>
2022-01-25 12:29:55 +0000

    config redis in settings

--- core/settings.py
+++ core/settings.py
@@ -132,3 +132,14 @@ BRAINTREE_CONF = braintree.Configuration(
     config('BRAINTREE_PRIVATE_KEY'),
 )
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
+
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = 6379
+# REDIS_DB = 1
+
